13dc716b0a4264f86313a8f40a70b73eb0bd1aea
camilor96 <juancamilo9607@gmail.com>
2020-05-04 01:18:26 -0500

    Componente infro portafolio

--- src/app/components/info-portafolio/info-portafolio.component.ts
+++ src/app/components/info-portafolio/info-portafolio.component.ts
@@ -6,8 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./info-portafolio.component.css']
 })
 export class InfoPortafolioComponent implements OnInit {
+  public title : string;
+  public description: string;
+  public year: number;
+  public empresa: string;
 
-  constructor() { }
+  constructor() { 
+    this.title = "Título del Proyecto"
+    this.description= "Desarrollo de pagina web para la empresa X";
+    this.year= 2019;
+    this.empresa = "HighForce"
+  }
 
   ngOnInit(): void {
   }
